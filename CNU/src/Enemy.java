367d39da982aca9fdd84bef20ac6c268ec2348d3
lhh9799 <77262005+lhh9799@users.noreply.github.com>
2021-06-06 15:42:15 +0900

    [프로토타입3]
    버튼 줄바꿈 구현

--- CNU/src/Enemy.java
+++ CNU/src/Enemy.java
@@ -1,15 +1,18 @@
 import java.util.Vector;
 
+//스킬정보(스킬명, 최소 대미지, 최대 대미지, 확률, 스킬의 남은 사용횟수)를 저장하는 클래스
 class Skill {	
-	String name;
-	int minDamage, maxDamage, probability, limit;
+	String name;			//스킬명 변수
+	int minDamage, maxDamage, probability, limit;	//순서대로 최소 대미지, 최대 대미지, 확률, 스킬의 남은 사용 횟수
 	
+	//사용자의 스킬 인스턴스를 만들 때 호출되는 생성자(스킬명, 최소 대미지, 최대 대미지, 스킬의 남은 사용 횟수를 초기화)
 	public Skill(String name, int minDamage, int maxDamage, int limit) {
 		this.name = name;
 		this.minDamage = minDamage;
 		this.maxDamage = maxDamage;
 		this.limit = limit;
 	}
+	//적의 스킬 인스턴스를 만들 때 호출되는 생성자(스킬명, 최소 대미지, 최대 대미지, 발동 확률을 초기화)
 	public Skill(String name, int minDamage, int maxDamage, String probability) {
 		this.name = name;
 		this.minDamage = minDamage;
@@ -18,17 +21,19 @@ class Skill {
 	}
 }
 
+//과제, 퀴즈, 시험클래스의 부모 클래스(객체의 이름, 이미지 경로, 체력, 스킬정보를 저장한다.)
 class Enemy {
-	String name, imgPath;
-	int hp;
-	Vector<Skill> skillSet = new Vector<Skill>();
+	String name, imgPath;	//순서대로 객체의 이름, 이미지 경로
+	int hp;					//체력 변수
+	Vector<Skill> skillSet = new Vector<Skill>();	//스킬정보를 저장하는 벡터
 	
+	//적이 살아있는지 여부를 리턴하는 메소드
 	public boolean isEnemyAlive() {
-		if(this.hp > 0)
-			return true;
-		else {
-			System.out.println("적이 사망했습니다!");
-			return false;
+		if(this.hp > 0)		//호출한 인스턴스의 멤버변수 hp가 0 이상이라면
+			return true;	//true 리턴
+		else {				//아니라면
+			System.out.println("적이 사망했습니다!");	//콘솔에 사망 메시지 출력
+			return false;	//false 리턴
 		}
 	}
 	
@@ -37,24 +42,25 @@ class Enemy {
 		return (int)(Math.random()*100);
 	}
 	
-//	public void attack(Player p) {
+	//적이 플레이어를 공격하는 함수
 	public String attack(Player p) {
-		int random = (int)(Math.random()*99 + 1);
+		int random = (int)(Math.random()*99 + 1);							//1부터 100까지의 랜덤한 숫자를 뽑음
 		int choice = (random < skillSet.elementAt(0).probability) ? 0 : 1;	//random이 0번스킬의 확률보다 작으면 0번 크면 1번 스킬 선택
-		String skill_name = skillSet.elementAt(choice).name;	//선택한 스킬의 이름 추출
-		int maxDamage = skillSet.elementAt(choice).maxDamage;
-		int minDamage = skillSet.elementAt(choice).minDamage;
-		int damageRange = maxDamage - minDamage + 1;			//대미지 폭 계산
-		int actualDamage = (int)(Math.random() * damageRange + minDamage);
+		String skill_name = skillSet.elementAt(choice).name;				//선택한 스킬의 이름 추출
+		int maxDamage = skillSet.elementAt(choice).maxDamage;				//선택한 스킬의 최대 대미지를 maxDamage 변수에 저장
+		int minDamage = skillSet.elementAt(choice).minDamage;				//선택한 스킬의 최소 대미지를 minDamage 변수에 저장
+		int damageRange = maxDamage - minDamage + 1;						//대미지 폭(max - min + 1) 계산
+		int actualDamage = (int)(Math.random() * damageRange + minDamage);	//적이 플레이어에게 줄 대미지 계산
 		
-		p.hp -= actualDamage;
+		p.hp -= actualDamage;												//플레이어의 체력 -= 적이 가한 대미지
+		//JLabel에 표시할 전투 정보를 리턴
 		return this.name + "의 " + skill_name + "을(를) 발동! " + p.name + "은(는) " + actualDamage + "의 피해를 입었습니다!";
-//		System.out.println(this.name + "의 " + skill_name + "을(를) 발동! " + p.name + "은(는) " + actualDamage + "의 피해를 입었습니다!");
 	}
 }
 
 //과제 클래스. 스킬이 없으므로 attack을 Override 하지 않음
 class Assignment extends Enemy {
+	//기본 생성자(과제 클래스의 인스턴스의 이름, 체력, 이미지 경로를 초기화)
 	public Assignment() {
 		this.name = "과제";
 		this.hp = 10;
@@ -68,6 +74,7 @@ class Assignment extends Enemy {
 
 //퀴즈 클래스
 class Quiz extends Enemy {
+	//기본 생성자(퀴즈 클래스의 인스턴스의 이름, 체력, 이미지 경로를 초기화)
 	public Quiz() {
 		this.name = "퀴즈";
 		this.hp = 20;
@@ -81,6 +88,7 @@ class Quiz extends Enemy {
 
 //기말고사 클래스
 class Exam extends Enemy {
+	//기본 생성자(시험 클래스의 인스턴스의 이름, 체력, 이미지 경로를 초기화)
 	public Exam() {
 		this.name = "시험";
 		this.hp = 30;
--- CNU/src/GameActivity.java
+++ CNU/src/GameActivity.java
@@ -1,285 +1,138 @@
-import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.image.FilteredImageSource;
-import java.awt.image.ImageFilter;
-import java.awt.image.ImageProducer;
-import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.swing.GrayFilter;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
-import javax.swing.plaf.FontUIResource;
+
 public class GameActivity extends JPanel {
-	private JLabel lbl, lbl2;
-	private JLabel console;
-	private JButton btn1, btn2, btn3, playerSkillButton[];
-	private JLabel playerNameLabel, enemyNameLabel;			//각 객체의 이름 라벨
-	private JLabel playerHPLabel, enemyHPLabel;					//각 객체의 체력 라벨
-	private JPanel playerPanel, enemyPanel;				//그룹화
-	private LinkedList<Enemy> enemyLinkedList = new LinkedList<Enemy>();
-	private ImageIcon playerImage, enemyImage;
-	private JLabel playerImageLabel, enemyImageLabel;
-	private JPanel playerSkillButtonPanel;
-	private Player player;	//플레이어 선언
-	private Image colorImage, gImage;
-	private ImageProducer producer;
-	private ImageFilter filter;
-	private Music Music;
-	
-//	public void updateEnemy() {
-//		//링크드 리스트에서 적 정보 가져옴
-//		Enemy e = enemyLinkedList.getFirst();
-//		
-//		//이미지 업데이트
-//		ImageIcon newEnemyImage = new ImageIcon(e.imgPath);
-//		enemyImageLabel.setIcon(newEnemyImage);
-//		//이름 라벨 업데이트
-//		enemyNameLabel.setText(e.name);
-//		//체력 라벨 업데이트
-//		enemyHPLabel.setText("HP: " + Integer.toString(e.hp));	//e1의 체력을 문자열로 변환 후 Label에 업데이트
-//	}
-		
-	public void goGray() {
-		System.out.println("goGray 호출됨");
-		//링크드 리스트에서 적 정보 가져옴
-		Enemy e = enemyLinkedList.getFirst();
-		ImageIcon colorImageIcon = new ImageIcon(e.imgPath);
-//		ImageIcon colorImageIcon = new ImageIcon("newImg/bongbi.png");
-		colorImage = colorImageIcon.getImage();
-		
-		//흑백으로 변경
-		ImageFilter filter = new GrayFilter(true, 50);
-		ImageProducer producer = new FilteredImageSource(colorImage.getSource(), filter);
-		Image gImage = Toolkit.getDefaultToolkit().createImage(producer);
-		
-		enemyImageLabel.setIcon(new ImageIcon(gImage));
-//		enemyImageLabel.setIcon(colorImageIcon);
-		try {
-			Thread.sleep(400);			//스레드 일시중지 1000ms(=1s)
-		}
-		catch (InterruptedException exc) {			//스레드가 중단되었을 때 발생하는 오류. 대기중인 스레드가 깨어나지 못할 때 발생
-			System.out.println(exc.getMessage());
-		}
-	}
+	private JLabel lbl1, lbl2;							//전투 정보를 표시할 레이블
+	private JLabel playerNameLabel, enemyNameLabel;		//플레이어, 적의 이름 레이블
+	private JLabel playerHPLabel, enemyHPLabel;			//플레이어, 적의 체력 레이블
+	private JButton playerSkillButton[];				//플레이어의 스킬 버튼 배열
+	private JPanel playerSkillButtonPanel;				//플레이어의 스킬 버튼들을 담는 패널
+	private JPanel playerPanel, enemyPanel;				//플레이어, 적의 정보를 저장하는 컴포넌트들을 담는 패널 
+	private ImageIcon playerImage, enemyImage;			//플레이어, 적의 이미지 아이콘
+	private JLabel playerImageLabel, enemyImageLabel;	//플레이어, 적의 이미지 아이콘이 담길 레이블
+	private Player player = new Player();				//플레이어 인스턴스 선언 및 생성
+	private LinkedList<Enemy> enemyLinkedList = new LinkedList<Enemy>();	//적들이 저장될 링크드 리스트(특징: 노드의 삭제가 용이함)
+	private Music Music;								//전투 BGM 객체
 	
 	public GameActivity(MyJPanel win) {
-		System.out.println("게임 액티비티 시작됨");
-		setLayout(null);
-
-		player = new Player();
+		setLayout(null);		//배치관리자를 null로 지정(모든 컴포넌트의 위치를 직접 지정)
 		
-		//적의 정보 링크드 리스트에 추가 - 삭제대기
-		//적(과제, 중간, 기말), 플레이어 선언 및 링크드 리스트에 추가
+		//적(과제, 중간, 기말) 인스턴스 생성 및 링크드 리스트에 추가
 		enemyLinkedList.add(new Assignment());
 		enemyLinkedList.add(new Quiz());
 		enemyLinkedList.add(new Exam());
 
-		//라벨 설정(전투 정보 표시)
-		lbl = new JLabel();
-		lbl.setBounds(200, 210, 500, 50);
-		lbl.setText("게임을 시작합니다");
-		lbl.setHorizontalAlignment(SwingConstants.CENTER); //수평 가운데 정렬
-		add(lbl);
+		//레이블1 설정(전투 정보 표시1)
+		lbl1 = new JLabel();
+		lbl1.setBounds(200, 210, 500, 50);
+		lbl1.setText("게임을 시작합니다");
+		lbl1.setHorizontalAlignment(SwingConstants.CENTER); //수평 가운데 정렬
+		add(lbl1);
 
-		//라벨2 설정(전투 정보 표시2)
+		//레이블2 설정(전투 정보 표시2)
 		lbl2 = new JLabel();
 		lbl2.setBounds(200, 240, 500, 50);
 		lbl2.setText(player.name + "의 체력은 " + player.hp + "입니다");
 		lbl2.setHorizontalAlignment(SwingConstants.CENTER); //수평 가운데 정렬
 		add(lbl2);
 		
-//		//전투 정보 표시
-//		console = new JLabel("<html>전투 정보<br><html/>");
-		
-//		Music Music = new Music("Pokemon_Battle.mp3", true);
-		
+		//플레이어의 스킬 버튼이 눌렸을 때 작동하는 액션리스너
 		class SkillListener implements ActionListener {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-//				try {
-//				goGray();
-//				System.out.println("94번");
-					JButton b = (JButton) e.getSource();
-					String selectedSkillName = b.getText();
-					Enemy currentEnemy = enemyLinkedList.getFirst();
-					
-					
-					for(var it : player.skillSetVect) {
-						//버튼에 적힌 스킬 이름과 일치하는 인스턴스 찾음 -> 플레이어의 공격 이어짐
-						if(it.name.equals(selectedSkillName)) {
-							if(it.limit > 0) {	//스킬 횟수 남음
-//								goGray();
-//								System.out.println("105번");
-								String ment = player.attack(currentEnemy, it);	//지금 싸우고 있는 상대를 넣어야 함
-								//왼쪽으로 20 이동
-								enemyImageLabel.setBounds(enemyImageLabel.getX() - 20, enemyImageLabel.getY(), enemyImageLabel.getWidth(), enemyImageLabel.getHeight());
-								//0.5초 후에 대상 슬라임을 원위치 시킨다.
-								new Timer().schedule(new TimerTask() {
-									@Override
-									public void run() {
-										enemyImageLabel.setBounds(enemyImageLabel.getX() + 20, enemyImageLabel.getY(), enemyImageLabel.getWidth(), enemyImageLabel.getHeight());
-									}
-								}, 500);
-								lbl.setText(ment);
-//								try {
-//								Thread.sleep(100);			//스레드 일시중지 1000ms(=1s)
-//								}
-//								catch (InterruptedException exc) {			//스레드가 중단되었을 때 발생하는 오류. 대기중인 스레드가 깨어나지 못할 때 발생
-//									System.out.println(exc.getMessage());
-//								}
-								break;
+			public void actionPerformed(ActionEvent e) {			//버튼 클릭 시 호출되는 메소드
+				JButton b = (JButton) e.getSource();				//클릭된 버튼을 알아냄
+				String selectedSkillName = b.getText();				//버튼의 문자열을 selectedSkillName 변수에 저장
+				Enemy currentEnemy = enemyLinkedList.getFirst();	//현재 대치하는 적의 정보를 currentEnemy 변수에 저장
+				
+				for(var it : player.skillSetVect) {					//for-each문으로 플레이어 스킬 벡터를 순회
+					//버튼에 적힌 스킬 이름과 일치하는 인스턴스 찾음 -> 플레이어의 공격 이어짐
+					if(it.name.equals(selectedSkillName)) {
+						if(it.limit > 0) {										//스킬의 남은 사용 횟수가 0보다 크다면(사용가능한 경우)
+							String report = player.attack(currentEnemy, it);	//현재 대치하는 적의 정보와 스킬의 정보를 인자로 넘김(report: 플레이어가 가한 대미지, 적의 남은 체력이 담긴 문자열)
+							//적의 이미지를 왼쪽으로 20 이동(피격 이펙트)
+							enemyImageLabel.setBounds(enemyImageLabel.getX() - 20, enemyImageLabel.getY(), enemyImageLabel.getWidth(), enemyImageLabel.getHeight());
+							//0.5초 후에 대상 슬라임을 원위치 시킨다.
+							new Timer().schedule(new TimerTask() {
+								@Override
+								public void run() {
+									enemyImageLabel.setBounds(enemyImageLabel.getX() + 20, enemyImageLabel.getY(), enemyImageLabel.getWidth(), enemyImageLabel.getHeight());
+								}
+							}, 500);
+							lbl1.setText(report);					//플레이어가 선택한 스킬명과 대미지, 적의 남은 체력을 레이블에 표시
+							
+							try {
+								Thread.sleep(100);					//스레드 일시중지 1000ms(=1s)
 							}
-							else {
-								System.out.println("플레이어 스킬 사용횟수 다 됨");
-								JOptionPane.showMessageDialog(null, "공격 횟수가 부족합니다.", "INFORMATION_MESSAGE", JOptionPane.INFORMATION_MESSAGE);
-								return;
+							catch (InterruptedException exc) {		//스레드가 중단되었을 때 발생할 수 있는 오류의 예외처리(대기중인 스레드가 깨어나지 못할 때 발생)
+								System.out.println(exc.getMessage());
 							}
+							break;									//다음 for문을 실행하는 것을 건너뜀(이미 버튼의 문자열과 일치하는 스킬정보를 찾았으므로)
+						}
+						else {										//스킬의 남은 사용 횟수가 0인 경우
+							System.out.println("스킬 사용횟수 부족");		//콘솔에 플레이어의 스킬 사용횟수가 없음을 알림
+							JOptionPane.showMessageDialog(null, "공격 횟수가 부족합니다.", "INFORMATION_MESSAGE", JOptionPane.INFORMATION_MESSAGE);
+							return;									//아래 코드 실행을 못 하도록 리턴
 						}
 					}
-					if(currentEnemy.isEnemyAlive()) {
-						String ment = currentEnemy.attack(player);	//적의 반격
-						lbl2.setText(ment);
-						playerHPLabel.setText("HP: " + Integer.toString(player.hp));
-						if(!player.isPlayerAlive()) {
-							System.out.println("플레이어 죽음. 게임 끝");
-							Music.close();	//음악 종료
-							win.change("DefeatEndingActivity", "GameActivity");
-//							System.exit(0);
-						}
-					}
-					//적이 죽음
-					else {
-						//흑백으로 변경
-						goGray();
-						System.out.println("144번");
-						enemyLinkedList.removeFirst();	//링크드 리스트 첫 번째 노드 삭제
-//						Thread.sleep(400);			//스레드 일시중지 1000ms(=1s)
-						if(enemyLinkedList.isEmpty()) {
-							//게임 끝 -> 다음 화면으로 넘어가기
-							System.out.println("적 모두 처치. 게임 끝");
-							Music.close();	//음악 종료
-							win.change("VictoryEndingActivity", "GameActivity");
-//							System.exit(0);
-						}
-					}
-					updateEnemy();
 				}
-//				catch (InterruptedException exc) {			//스레드가 중단되었을 때 발생하는 오류. 대기중인 스레드가 깨어나지 못할 때 발생
-//					System.out.println(exc.getMessage());
-//				}
-//			}
+				
+				//아래 코드는 플레이어의 스킬이 사용된 후 실행(남은 사용 횟수가 부족하여 attack 함수가 실행되지 않는 경우 실행되지 않음)
+				if(currentEnemy.isEnemyAlive()) {					//적이 살아있다면 -> 적의 반격
+					String report = currentEnemy.attack(player);	//적의 attack 함수 호출(report: 적이 가한 대미지, 플레이어의 남은 체력이 담긴 문자열)
+					lbl2.setText(report);							//적이 사용한 스킬명과 대미지, 플레이어의 남은 체력을 레이블에 표시
+					playerHPLabel.setText("HP: " + Integer.toString(player.hp));	//플레이어의 체력 레이블 갱신
+					if(!player.isPlayerAlive()) {					//플레이어가 죽은 경우
+						System.out.println("플레이어 죽음. 게임 끝");		//콘솔에 플레이어가 죽음을 알림
+						Music.close();								//음악 종료
+						win.change("defeatEndingActivity");			//패배 엔딩으로 창 전환
+					}
+				}
+				
+				//적이 죽은 경우
+				else {
+					enemyLinkedList.removeFirst();					//링크드 리스트의 첫 번째 노드 삭제
+					if(enemyLinkedList.isEmpty()) {					//링크드 리스트가 비어있다면(더 이상 나타날 적이 없음)
+						System.out.println("적 모두 처치. 게임 끝");		//콘솔에 게임이 종료됨을 알림
+						Music.close();								//음악 종료
+						win.change("victoryEndingActivity");		//승리 엔딩으로 창 전환
+					}
+				}
+				changeEnemy();
+				}
 		}
 		
-		//남은 스킬 횟수 + 대미지 JLabel에 표기
-		//적 교체 작동안함
-		//적 사망 멘트 추가
-		//봉비 체력 업데이트 안됨
-		//로그인 시도 이후 일어나는 액션
-//		class SkillListener implements ActionListener {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				try {
-//					JButton b = (JButton) e.getSource();
-//					String selectedSkillName = b.getText();
-//					Enemy currentEnemy = enemyLinkedList.getFirst();
-//					
-//					goGray();
-//					
-//					for(var it : player.skillSetVect) {
-//						//버튼에 적힌 스킬 이름과 일치하는 인스턴스 찾음 -> 플레이어의 공격 이어짐
-//						if(it.name.equals(selectedSkillName)) {
-//							if(it.limit > 0) {	//스킬 횟수 남음
-//								String ment = player.attack(currentEnemy, it);	//지금 싸우고 있는 상대를 넣어야 함
-//								//왼쪽으로 20 이동
-//								enemyImageLabel.setBounds(enemyImageLabel.getX() - 20, enemyImageLabel.getY(), enemyImageLabel.getWidth(), enemyImageLabel.getHeight());
-//								//0.5초 후에 대상 슬라임을 원위치 시킨다.
-//								new Timer().schedule(new TimerTask() {
-//									@Override
-//									public void run() {
-//										enemyImageLabel.setBounds(enemyImageLabel.getX() + 20, enemyImageLabel.getY(), enemyImageLabel.getWidth(), enemyImageLabel.getHeight());
-//									}
-//								}, 500);
-//								lbl.setText(ment);
-////								Thread.sleep(100);			//스레드 일시중지 1000ms(=1s)
-//								break;
-//							}
-//							else {
-//								System.out.println("플레이어 스킬 사용횟수 다 됨");
-//								return;
-//							}
-//						}
-//					}
-//					if(currentEnemy.isEnemyAlive()) {
-//						String ment = currentEnemy.attack(player);	//적의 반격
-//						lbl2.setText(ment);
-//						playerHPLabel.setText("HP: " + Integer.toString(player.hp));
-//						if(!player.isPlayerAlive()) {
-//							System.out.println("플레이어 죽음. 게임 끝");
-//							System.exit(0);
-//						}
-//					}
-//					//적이 죽음
-//					else {
-//						//흑백으로 변경
-//						goGray();
-//						Thread.sleep(3000);			//스레드 일시중지 1000ms(=1s)
-//						enemyLinkedList.removeFirst();	//링크드 리스트 첫 번째 노드 삭제
-////							Thread.sleep(400);			//스레드 일시중지 1000ms(=1s)
-//						if(enemyLinkedList.isEmpty()) {
-//							//게임 끝 -> 다음 화면으로 넘어가기
-//							System.out.println("적 모두 처치. 게임 끝");
-//							System.exit(0);
-//						}
-//					}
-//					changeEnemy();
-//				}
-//				catch (InterruptedException exc) {			//스레드가 중단되었을 때 발생하는 오류. 대기중인 스레드가 깨어나지 못할 때 발생
-//					System.out.println(exc.getMessage());
-//				}
-//			}
-//		}
-		
-		//플레이어 스킬 Button에 텍스트 지정 및 버튼 패널에 추가
+		//플레이어 스킬 버튼을 담을 패널
 		playerSkillButtonPanel = new JPanel();
-		playerSkillButtonPanel.setLayout(new FlowLayout());
+		playerSkillButtonPanel.setLayout(new FlowLayout());			//FlowLayout을 사용(왼쪽에서 오른쪽, 위에서 아래로 배치)
 		playerSkillButtonPanel.setBorder(new LineBorder(Color.LIGHT_GRAY, 3));
-		playerSkillButtonPanel.setBounds(10, 350, 200, 80);
-		int numberOfPlayerSkills = player.skillSetVect.size();	//스킬의 가짓수
-		playerSkillButton = new JButton[numberOfPlayerSkills];
+		playerSkillButtonPanel.setBounds(10, 350, 250, 100);
+		int numberOfPlayerSkills = player.skillSetVect.size();		//벡터의 크기 == 플레이어 스킬의 가짓수
+		playerSkillButton = new JButton[numberOfPlayerSkills];		//플레이어 스킬의 가짓수만큼 버튼 배열 할당
 		
-		for(int i=0; i<player.skillSetVect.size(); i++) {
-			playerSkillButton[i] = new JButton();
-			Skill s = player.skillSetVect.elementAt(i);
-//			String SkillName = s.name;
-//			String SkillLimit = s.limit;
-//			String ment = s.name + "\n" + "잔여 횟수: " + s.limit;
-//			playerSkillButton[i].setText(s.name + "<html><br><html/>" + s.limit);
-//			console = new JLabel("<html>전투 정보<br><html/>");
-			playerSkillButton[i].setText(s.name);
-			playerSkillButton[i].addActionListener(new SkillListener());	//리스너 구현하기
-			playerSkillButtonPanel.add(playerSkillButton[i]);
+		for(int i=0; i<player.skillSetVect.size(); i++) {			//스킬의 수 번 반복하여
+			playerSkillButton[i] = new JButton();					//i번째 버튼 인스턴스 생성
+			Skill s = player.skillSetVect.elementAt(i);				//플레이어 스킬 벡터의 i번째 원소를 Skill형 변수 s에 저장
+			playerSkillButton[i].setText(s.name);					//s에서 스킬명을 추출해 i번째 버튼의 텍스트로 지정
+			playerSkillButton[i].addActionListener(new SkillListener());	//i번째 버튼에 리스너 등록
+			playerSkillButton[i].setBackground(new Color(237, 247, 245));	//버튼에 색 설정
+			playerSkillButton[i].setFont(new Font("맑은 고딕", Font.BOLD, 15));
+			playerSkillButtonPanel.add(playerSkillButton[i]);		//플레이어 스킬 버튼을 담는 패널에 버튼 부착
 		}
-		add(playerSkillButtonPanel);				//버튼섬을 패널에 추가
+		add(playerSkillButtonPanel);								//플레이어 스킬 버튼을 담는 패널을 GameActivity패널에 추가
 		
 		//=======플레이어 패널=======
 		playerPanel = new JPanel();
@@ -287,85 +140,74 @@ public class GameActivity extends JPanel {
 		playerPanel.setBounds(10, 150, 150, 150);
 		playerPanel.setBorder(new LineBorder(Color.LIGHT_GRAY, 2));
 		add(playerPanel);
-		//과제 패널에 추가할 이미지 - 이미지는 적의 종류에 따라 달라짐
+		//플레이어 패널에 추가할 이미지
 		playerImage = new ImageIcon(player.imgPath);
 		playerImageLabel = new JLabel(playerImage);
 		playerImageLabel.setBounds(25, 40, 100, 100);
 		playerImageLabel.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
 		playerPanel.add(playerImageLabel);
-		//과제 패널에 추가할 이름 라벨
+		//플레이어 패널에 추가할 이름 라벨
 		playerNameLabel = new JLabel();
-		playerNameLabel.setBounds(65, 0, 50, 20);		//이름 라벨 위치 설정
+		playerNameLabel.setBounds(65, 0, 50, 20);
 		playerNameLabel.setText(player.name);
 		playerPanel.add(playerNameLabel);
-		//과제 패널에 추가할 체력 라벨
-		playerHPLabel = new JLabel();			//과제의 체력 라벨 inflate
-		playerHPLabel.setBounds(60, 20, 50, 20);		//과제 라벨 위치 설정
-		playerHPLabel.setText("HP: " + Integer.toString(player.hp));	//e1의 체력을 문자열로 변환 후 Label에 업데이트
+		//플레이어 패널에 추가할 체력 라벨
+		playerHPLabel = new JLabel();
+		playerHPLabel.setBounds(60, 20, 50, 20);
+		playerHPLabel.setText("HP: " + Integer.toString(player.hp));	//플레이어의 체력을 문자열로 변환 후 Label에 업데이트
 		playerPanel.add(playerHPLabel);
 		
-		summonEnemy(enemyLinkedList.getFirst());	//첫 번재 몬스터 과제 추가
-//		gameStart();
-	}
-
-//	public void gameStart() {
-//		//적의 링크드 리스트
-//		while(!enemyLinkedList.isEmpty()) {
-//			Enemy e = enemyLinkedList.getFirst();
-//			summonMonster(e);
-////			if(!e.isEnemyAlive()) {
-////				enemyLinkedList.removeFirst();				
-////			}
-//			enemyLinkedList.removeFirst();
-//			System.out.println("실행중");
-//		}
-//	}
+		summonEnemy(enemyLinkedList.getFirst());	//첫 번재 적(과제)을 적 패널 추가
+	}	//GameActivity 생성자의 끝
 	
+	//플레이어의 이름을 변경하는 메소드(IntroActivity에서 확인버튼을 누르면 호출됨)
 	public void updatePlayerNameLabel(String Username) {
-		player.playerNameChange(Username);
-		playerNameLabel.setText(Username);
+		player.playerNameChange(Username);			//player 인스턴스의 이름 변경
+		playerNameLabel.setText(Username);			//플레이어의 이름 레이블 변경
 	}
 	
+	//링크드 리스트의 첫 번째 적을 패널에 추가하는 메소드 
 	public void summonEnemy(Enemy e) {
-		//=======적 패널 템플릿=======
+		//=======적 패널=======
 		enemyPanel = new JPanel();
 		enemyPanel.setLayout(null);
-//		enemyPanel.setBounds(10, 10, 150, 150);
 		enemyPanel.setBounds(450, 10, 150, 150);
 		enemyPanel.setBorder(new LineBorder(Color.LIGHT_GRAY, 2));
 		add(enemyPanel);
-		//과제 패널에 추가할 이미지 - 이미지는 적의 종류에 따라 달라짐
+		//적 패널에 추가할 이미지 - 이미지는 적의 종류에 따라 달라짐
 		enemyImage = new ImageIcon(e.imgPath);
 		enemyImageLabel = new JLabel(enemyImage);
 		enemyImageLabel.setBounds(25, 40, 100, 100);
 		enemyImageLabel.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
 		enemyPanel.add(enemyImageLabel);
-		//과제 패널에 추가할 이름 라벨
+		//적 패널에 추가할 이름 라벨
 		enemyNameLabel = new JLabel();
-		enemyNameLabel.setBounds(65, 0, 50, 20);		//이름 라벨 위치 설정
+		enemyNameLabel.setBounds(65, 0, 50, 20);
 		enemyNameLabel.setText(e.name);
 		enemyPanel.add(enemyNameLabel);
-		//과제 패널에 추가할 체력 라벨
-		enemyHPLabel = new JLabel();			//과제의 체력 라벨 inflate
-		enemyHPLabel.setBounds(60, 20, 50, 20);		//과제 라벨 위치 설정
-		enemyHPLabel.setText("HP: " + Integer.toString(e.hp));	//e1의 체력을 문자열로 변환 후 Label에 업데이트
+		//적 패널에 추가할 체력 라벨
+		enemyHPLabel = new JLabel();
+		enemyHPLabel.setBounds(60, 20, 50, 20);
+		enemyHPLabel.setText("HP: " + Integer.toString(e.hp));	//적의 체력을 문자열로 변환 후 Label에 업데이트
 		enemyPanel.add(enemyHPLabel);
 	}
 	
-	public void updateEnemy() {
+	//적 패널에 피격된 적의 정보를 갱신하는 메소드
+	public void changeEnemy() {
+		//링크드 리스트가 비어있지 않다면(적이 더 있다면) 링크드 리스트에서 적 정보 가져옴
 		if(!enemyLinkedList.isEmpty()) {
-			//링크드 리스트에서 적 정보 가져옴
 			Enemy e = enemyLinkedList.getFirst();
 			
-			//이미지 업데이트
+			//이미지 업데이트(적이 사망한 경우 갱신 필요)
 			ImageIcon newEnemyImage = new ImageIcon(e.imgPath);
 			enemyImageLabel.setIcon(newEnemyImage);
 			//이름 라벨 업데이트
 			enemyNameLabel.setText(e.name);
 			//체력 라벨 업데이트
-			enemyHPLabel.setText("HP: " + Integer.toString(e.hp));	//e1의 체력을 문자열로 변환 후 Label에 업데이트
+			enemyHPLabel.setText("HP: " + Integer.toString(e.hp));	//적의 체력을 문자열로 변환 후 Label에 업데이트
 		}
 	}
+	
 	public void init() {
 		Music = new Music("music/poke.mp3", true);
 		Music.start();
